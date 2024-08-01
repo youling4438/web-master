59266853f949202638482eef065e182e3612f32a
youling4438 <youling0106@163.com>
2024-08-01 11:09:01 +0800

    fix cf build issue

--- app/(main)/feed.xml/middleware.ts
+++
@@ -1,5 +0,0 @@
-import { getLatestBlogPosts } from '~/sanity/queries'
-export const middleware = async () => {
-    // Fetch any necessary data here 
-    return await getLatestBlogPosts({ limit: 999 });
-};
